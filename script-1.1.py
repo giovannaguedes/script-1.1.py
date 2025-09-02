204303ecfc0ad7e6c2e90134e0f177158a203645
giovannaguedes <154679630+giovannaguedes@users.noreply.github.com>
2025-09-02 14:17:23 -0300

    script-1.1.py

---
+++ script-1.1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#Meu primeiro script
+print("M!ss s3c")
+
+ip = input("Digite o IP: ")
+porta = int(input("Digite a porta: "))
+ver = 1.1
+
+print ("IP -", type(ip)) 
+print ("Porta , type(porta))
+print ("Ver - ", type(ver))
+print ("Varrendo Host: %s na porta %d" %(ip, porta))
